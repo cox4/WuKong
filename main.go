22145db480606569b2cba67b4a2624a4521135e6
cox4 <lyhlcong@gmail.com>
2018-03-29 08:36:01 -0400

    Change to new log

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.229.26.233:9200"
+	ES_URL = "http://35.231.152.179:9200"
 )
 
 
